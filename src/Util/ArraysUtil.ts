964cd8d1c5144d491b13952b91cf84ef0ed46478
rajat545 <rajatpandit545@gmail.com>
2023-05-02 23:36:42 +0530

    i had been changed all useless tag or error in css

--- src/Util/ArraysUtil.ts
+++
@@ -1,6 +0,0 @@
-export const  sameMembers = (arr1: string[], arr2: string[])  =>{
-    const set1 = new Set(arr1);
-    const set2 = new Set(arr2);
-    return arr1.every(item => set2.has(item)) &&
-        arr2.every(item => set1.has(item))
-}
--- src/Util/GifUtil.ts
+++
@@ -1,9 +0,0 @@
-export const DISCO_GIF = ""
-
-export const UNLOCK_THE_FUNK_GIF = ""
-
-export const ROTATING_VIAL_V2 = ""
-
-export const FUNKY_POINTS_GIF = ""
-
-export const MATIC_GIF = ""
